eb33c2128fbba8ee145cb8ede9aa4c6143ea1c79
Marlon Leon <ghostveene@gmail.com>
2023-01-02 14:55:04 -0500

    added contents from header and maincontent files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Header } from './Header';
+import { MainContent } from './MainContent';
+
+export function App() {
+    return (
+        <div className="container">
+            <Header />
+            <MainContent />
+        </div>
+    )
+}
