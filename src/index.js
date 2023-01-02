e8676dd71eb0de9c55e7ee8c71f80e91d9bfe338
Marlon Leon <ghostveene@gmail.com>
2023-01-02 13:14:14 -0500

    seperated multiple components to there own files

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 import React, { createElement } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import logo from './imgs/React-logo.png'
+import { Header } from './Header';
+import { MainContent } from './MainContent';
+import { Footer } from './Footer';
 
-const mainPage = (
-  <div>
-    <img src={logo} width="40px" alt="react logo" ></img>
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-)
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(mainPage)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Page />)
 
 
 
