1a0594892a7cbf5f2c5198621761a8551d92b271
Marlon Leon <ghostveene@gmail.com>
2023-01-02 14:55:39 -0500

    seperated header and main content into app.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Header } from './Header';
-import { MainContent } from './MainContent';
-
-function App() {
-    return (
-        <div className="container">
-            <Header />
-            <MainContent />
-        </div>
-    )
-}
+import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />)
