c9d44dbbffdabc5c4b9cf2956065911287dc4d27
Marlon Leon <ghostveene@gmail.com>
2023-01-02 15:24:24 -0500

    changed class name for unordered-list

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -2,7 +2,7 @@ export function MainContent() {
     return (
         <div className="main-content">
             <h1>Fun facts about React</h1>
-            <ul className="unorder-list">
+            <ul className="unordered-list">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100k stars on GitHub</li>
