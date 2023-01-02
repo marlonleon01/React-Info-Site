da89e733d0d89b1e938fba6b1782a6206e9b8713
Marlon Leon <ghostveene@gmail.com>
2023-01-02 13:13:38 -0500

    seperated maincontent from index.js to its own file

---
+++ src/MainContent.js
@@ -0,0 +1,12 @@
+export function MainContent() {
+    return (
+        <div className="main-content">
+            <h1>Reasons I'm excited to learn React!</h1>
+            <ol className="order-list">
+                <li>To help me become a better programer.</li>
+                <li>To simplify writing code.</li>
+                <li>To learn something new.</li>
+            </ol>
+        </div>
+    )
+}
