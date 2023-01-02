6b56837facf732b9df920c43099f37784ac5d35a
Marlon Leon <ghostveene@gmail.com>
2023-01-02 11:21:17 -0500

    render main text

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import logo from './imgs/React-logo.png'
 
-
+const mainPage = (
+  <div>
+    <img src={logo} width="40px" alt="react logo" ></img>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(mainPage)
 
 
 
