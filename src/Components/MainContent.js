17fb66845aaf9e5d4a29bedc2a00085d4ab271ad
Marlon Leon <ghostveene@gmail.com>
2023-01-30 06:51:25 -0500

    added main section

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -1,7 +1,7 @@
-export function MainContent() {
+export function MainContent(props) {
     return (
-        <div className="main-content">
-            <h1>Fun facts about React</h1>
+        <main className={props.darkMode ? "dark" : ""}>
+            <h1 className="main-title">Fun facts about React</h1>
             <ul className="unordered-list">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
@@ -9,6 +9,6 @@ export function MainContent() {
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-        </div>
+        </main>
     )
 }
