ac7d823ef95fb1700711ce3ed6284af3aa0f519b
Marlon Leon <ghostveene@gmail.com>
2023-01-30 06:37:00 -0500

    fix to add darkmode toggle

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,17 @@
 import logo from '../imgs/React-logo.png'
 
-export function Header() {
+export function Header(props) {
     return (
-        <header>
-            <nav className="nav">
-                <img src={logo} className="small-react-logo"></img>
-                <span className="react-facts-title">ReactFacts</span>
-                <span className="react-course-description">React Course - Project 1</span>
+            <nav className={props.darkMode ? "dark": ""}>
+                <img src={logo} className="small-react-logo" />
+                <h3 className="react-facts-title">ReactFacts</h3>
+                <div className="toggler">
+                    <p className="toggler-light">Light</p>
+                    <div className="toggler-slider" onClick={props.toggleDarkMode}>
+                        <div className="toggler-slider-circle"></div>
+                    </div>
+                    <p className="toggler-dark">Dark</p>
+                </div>
             </nav>
-        </header>
     )
 }
