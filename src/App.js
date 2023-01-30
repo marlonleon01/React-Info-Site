bc4ca40a6030af49ac0ff66879d7ceca3623af17
Marlon Leon <ghostveene@gmail.com>
2023-01-30 07:16:24 -0500

    setState to add darkmode prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { Header } from './Components/Header';
 import { MainContent } from './Components/MainContent';
+import { useState } from "react"
 
 export function App() {
+    const [darkMode, setDarkMode] = useState(false)
+    
     return (
         <div className="container">
-            <Header />
-            <MainContent />
+            <Header 
+                darkMode={darkMode}
+            />
+            <MainContent 
+                darkMode={darkMode}
+            />
         </div>
     )
 }
